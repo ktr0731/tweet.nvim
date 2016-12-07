81fe2a2f6d85b81a9a457e5183244d1742f978bb
Taro Aoki <aizu.s1230022@gmail.com>
2016-12-07 16:58:56 +0900

    [WIP]

--- rplugin/python3/tweet/__init__.py
+++ rplugin/python3/tweet/__init__.py
@@ -1,3 +1,6 @@
+import urllib
+import urllib2
+
 import neovim
 
 
@@ -5,7 +8,24 @@ import neovim
 class TestPlugin(object):
     def __init__(self, nvim):
         self.nvim = nvim
+        self.params = {
+                    'oauth_token': '',
+                }
 
-    @neovim.command("Hello")
-    def hello(self):
-        self.nvim.current.line = "Hello!!!!!!!!!"
+    @neovim.command('Tweet', nargs='*')
+    def hello(self, lines):
+        end_point = 'https://api.twitter.com/1.1/statuses/update.json'
+
+        content = ''
+        for line in lines:
+            content += line + '\n'
+
+        params = this.params
+        params.update({'status': content})
+
+        req = urllib2.Request(end_point)
+        req.add_header('application/x-www-form-urlencoded')
+        req.add_data(urllib.urlencode(params))
+        res = urllib2.urlopen(req)
+
+        self.nvim.current.line = 'Hello!!!!{0}'.format(content)
