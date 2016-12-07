bff7c544c93fe70c11adea1372f3ede8ae1d2a78
Taro Aoki <aizu.s1230022@gmail.com>
2016-12-07 19:18:16 +0900

    [ADD] Makefile

--- rplugin/python3/tweet/__init__.py
+++ rplugin/python3/tweet/__init__.py
@@ -1,31 +1,39 @@
-import urllib
-import urllib2
+# -*- coding: utf-8 -*-
 
+import os
+
+from requests_oauthlib import OAuth1Session
 import neovim
 
 
 @neovim.plugin
-class TestPlugin(object):
+class TweetNvim(object):
     def __init__(self, nvim):
+        keys = ['CONSUMER', 'CONSUMER_SECRET', 'ACCESS_TOKEN', 'ACCESS_TOKEN_SECRET']
+        self.keys = {}
+        for key in keys:
+            if os.getenv('TWEET_NVIM_' + key) is None:
+                exit(1)
+            else:
+                self.keys[key] = os.getenv('TWEET_NVIM_' + key)
+
         self.nvim = nvim
-        self.params = {
-                    'oauth_token': '',
-                }
 
     @neovim.command('Tweet', nargs='*')
-    def hello(self, lines):
-        end_point = 'https://api.twitter.com/1.1/statuses/update.json'
+    def tweet(self, lines):
+        url = 'https://api.twitter.com/1.1/statuses/update.json'
 
         content = ''
         for line in lines:
             content += line + '\n'
 
-        params = this.params
-        params.update({'status': content})
+        twitter = OAuth1Session(
+                    self.keys[0],
+                    self.keys[1],
+                    self.keys[2],
+                    self.keys[3]
+                )
 
-        req = urllib2.Request(end_point)
-        req.add_header('application/x-www-form-urlencoded')
-        req.add_data(urllib.urlencode(params))
-        res = urllib2.urlopen(req)
+        params = {'status': content}
 
-        self.nvim.current.line = 'Hello!!!!{0}'.format(content)
+        r = twitter.post(url, params=params)
