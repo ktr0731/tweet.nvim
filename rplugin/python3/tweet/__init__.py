c5b5336ad3facc7953dea520cec46f53eb89324c
Taro Aoki <aizu.s1230022@gmail.com>
2016-12-10 13:14:18 +0900

    [MOD] Bind a variable "twitter" to self

--- rplugin/python3/tweet/__init__.py
+++ rplugin/python3/tweet/__init__.py
@@ -9,21 +9,35 @@ import neovim
 @neovim.plugin
 class TweetNvim(object):
     def __init__(self, nvim):
-        keys = ['CONSUMER', 'CONSUMER_SECRET', 'ACCESS_TOKEN', 'ACCESS_TOKEN_SECRET']
-        self.keys = {}
+        keys = {
+                'CONSUMER': '',
+                'CONSUMER_SECRET': '',
+                'ACCESS_TOKEN': '',
+                'ACCESS_TOKEN_SECRET': ''
+                }
 
-        for key in keys:
-            if os.getenv('TWEET_NVIM_' + key) is None:
+        for key in keys.keys():
+            # TODO: Vim 変数も確認する
+            if os.getenv('TWEET_NVIM_{}'.format(key)) is None:
                 raise Exception('Required environment variables are missing!')
             else:
-                self.keys[key] = os.getenv('TWEET_NVIM_' + key)
+                keys[key] = os.getenv('TWEET_NVIM_{}'.format(key))
+
+        self.twitter = OAuth1Session(
+                    keys['CONSUMER'],
+                    keys['CONSUMER_SECRET'],
+                    keys['ACCESS_TOKEN'],
+                    keys['ACCESS_TOKEN_SECRET'])
 
         self.nvim = nvim
+
+    def echo(self, message):
+        self.nvim.command("echo '[Tweet.nvim] {}'".format(message))
 
     @neovim.command('Tweet', nargs='*', sync=True)
     def tweet(self, lines):
         if len(lines) == 0:
-            raise Exception('Usage: Tweet [line...]')
+            self.echo('Usage: Tweet [line...]')
 
         url = 'https://api.twitter.com/1.1/statuses/update.json'
 
@@ -31,12 +45,13 @@ class TweetNvim(object):
         for line in lines:
             content += line + '\n'
 
-        twitter = OAuth1Session(
-                    self.keys['CONSUMER'],
-                    self.keys['CONSUMER_SECRET'],
-                    self.keys['ACCESS_TOKEN'],
-                    self.keys['ACCESS_TOKEN_SECRET'])
-
         params = {'status': content}
 
-        r = twitter.post(url, params=params)
+        r = self.twitter.post(url, params=params)
+        self.echo('Tweeted')
+
+    @neovim.command('TwitterTimeline')
+    def timeline(self):
+        self.nvim.command("setlocal splitright")
+        self.nvim.command("vnew")
+        self.nvim.command("setlocal buftype=nofile bufhidden=hide nowrap nolist nonumber nomodifiable")
